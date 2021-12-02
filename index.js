668d0a67721f24dd1ab6eb82fecb5e7ffa95a514
Ardizzone Pietro <ardizzone.pietro@osdbsesto>
2021-12-02 11:15:21 +0100

    send apiserver response

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ console.log(a+b); */
 var port = 3000;
 var host = "localhost";
 apiServer.listen(port, host, ()=>{
-    console.log("server running at http://%s:%s",
+    console.log("server running at http://%s:%d",
     host, port);
 });       //vediamo se è in ascolto
+
+apiServer.get("/", (request, response)=>{
+console.log("sono in get /", request);
+response.send("<h1>ciao client sei in home<h1>"); //risposta con anche tgag html
+
+})    //cio che do quando contatto il server (risosrsa, fuunction) in questo caso è la all
+
