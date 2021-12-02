7902fb3c04d180b753240d20ac2f2a5c80b3099c
Ardizzone Pietro <ardizzone.pietro@osdbsesto>
2021-12-02 11:43:42 +0100

    query nome

--- index.js
+++ index.js
@@ -8,18 +8,22 @@ var b = "3";
 console.log(a+b); */
 var port = 3000;
 var host = "localhost";
-apiServer.listen(port, host, ()=>{
+apiServer.listen(port, host, () => {
     console.log("server running at http://%s:%d",
-    host, port);
+        host, port);
 });       //vediamo se è in ascolto
 
-apiServer.get("/", (request, response)=>{
-console.log("sono in get /", request);
-response.send("<h1>ciao client sei in home<h1>"); //risposta con anche tgag html
+apiServer.get("/", (request, response) => {
+    console.log("sono in get /", request);
+    response.send("<h1>ciao client sei in home<h1>"); //risposta con anche tgag html
 
 })    //cio che do quando contatto il server (risosrsa, fuunction) in questo caso è la all
 
-apiServer.get("/nome", (request, response)=>{
-     console.log("richiesta nome");
-    response.send("<p1>Ardizzone Pietro<p1>"); 
-    })   
+apiServer.get("/nome", (request, response) => {
+    console.log("richiesta nome");
+    response.send("<p1>Ardizzone Pietro<p1>");
+})
+apiServer.get("/mioNome", (request, response) => {
+    console.log(request.query.nome);
+    response.send("ciao, il mio nome è: "+request.query.nome);
+})   //prendo la stringa dall'urla di variabile nome
